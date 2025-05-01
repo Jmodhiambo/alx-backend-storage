ef46cca9588331fd4e4ba8e810fa4b5363410040
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-01 21:42:11 +0300

    Task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- Function: SafeDiv(a INT, b INT) - divides a by b or returns 0 if b is 0
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+        IF b = 0 THEN
+                RETURN 0;
+        ELSE
+                RETURN a / b;
+        END IF;
+END$$
+
+DELIMITER ;
