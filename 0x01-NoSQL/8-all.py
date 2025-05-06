15e29bf5cde46b76cb8518dad7d5e40904094943
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-07 02:36:26 +0300

    Task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+The Python function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection"""
+    if mongo_collection is None:
+        return []
+
+    return list(mongo_collection.find())
