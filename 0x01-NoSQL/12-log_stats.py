e1623ae33fcea0077ccc659df37b57f993719f3b
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-07 05:06:47 +0300

    Task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-'''Task 12's module.
-'''
+'''Provides stats about Nginx logs stored in MongoDB'''
+
 from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
+    '''Prints stats about Nginx request logs'''
+    print('{} logs'.format(len(list(nginx_collection.find()))))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
+        count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, count))
+    status_check_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
-    print('{} status check'.format(status_checks_count))
+    print('{} status check'.format(status_check_count))
 
 
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
+def main():
+    '''Connects to MongoDB and prints the log stats'''
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+    nginx_collection = client.logs.nginx
+    print_nginx_request_logs(nginx_collection)
 
 
 if __name__ == '__main__':
-    run()
+    main()
