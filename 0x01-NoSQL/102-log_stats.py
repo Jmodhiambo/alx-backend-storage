155cf55c96a7333a10451a260a699d0a39199617
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-07 05:44:34 +0300

    Task 15

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''Provides stats about Nginx logs stored in MongoDB'''
+
+from pymongo import MongoClient
+
+
+def print_nginx_request_logs(nginx_collection):
+    '''Prints stats about Nginx request logs'''
+    print('{} logs'.format(len(list(nginx_collection.find()))))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, count))
+    status_check_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_check_count))
+
+    print("IPs:")
+    top_ips = nginx_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in top_ips:
+        print("\t{}: {}".format(ip["_id"], ip["count"]))
+
+
+def main():
+    '''Connects to MongoDB and prints the log stats'''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print_nginx_request_logs(nginx_collection)
+
+
+if __name__ == '__main__':
+    main()
